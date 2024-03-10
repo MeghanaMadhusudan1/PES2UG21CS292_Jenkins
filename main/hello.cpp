61003bede9fa6acf6fed20883f12927c6bd90784
Meghana_M <96113243+MeghanaMadhusudan1@users.noreply.github.com>
2024-03-10 15:16:28 +0530

    Describe this committ

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World,this is srn cs292\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and run this\n";
   return 0;
 }
