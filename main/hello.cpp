7f01ec51c36645be434290c44946e7d2721b0287
Meghana_M <96113243+MeghanaMadhusudan1@users.noreply.github.com>
2024-03-10 15:20:11 +0530

    Describe this committ

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World,this is srn cs292\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this\n";
+  cout << "webhook successfulky added\n";
   return 0;
 }
